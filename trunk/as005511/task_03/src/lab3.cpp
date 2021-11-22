60c9c4e27a55ea010a145e3741c5303fb887efab
Artyom Marzan <67014535+marzan-mars@users.noreply.github.com>
2021-11-22 20:20:41 +0300

    Add files via upload

---
+++ trunk/as005511/task_03/src/lab3.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : lab3.cpp
+// Author      : Marzan Artyom
+// Version     :
+// Copyright   : 
+// Description : completed the task on laboratory work 3 Marzan Artyom
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl; // prints Hello World!
+	return 0;
+}
